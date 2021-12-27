f7b55380757d2c78d91c0a25393ef3f39a3f0264
Sourabh Sethi <sourabhhsethii@linux.com>
2021-12-27 00:15:22 -0500

    Basic of Java, Main Method, Variable, Primitive Datatype.

---
+++ src/com/dxsys/code/mosh/basic/Main.java
@@ -0,0 +1,14 @@
+package com.dxsys.code.mosh.basic;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte myAge = 31;
+        int herAge = myAge;
+        long viewCounts = 3_123_456_789_1l;
+        System.out.println("ViewCounts " + viewCounts);
+        float price = 10.99f; // By Default java treat decimal number as double to make it float, need to used f in end.
+        char letter ='A';
+        boolean isEligile = false;
+    }
+}
