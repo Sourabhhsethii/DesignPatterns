bdc3ee125cd81de164adba189be75257c79a7615
Sourabh Sethi <sourabhhsethii@linux.com>
2021-12-30 12:32:43 -0500

    Overview of Build-in Functional Interfaces

--- src/com/dxsys/code/mosh/advance/lambdas/LambdasDemo.java
+++ src/com/dxsys/code/mosh/advance/lambdas/LambdasDemo.java
@@ -30,6 +30,16 @@ public class LambdasDemo {
          * //Class/Object::method
          */
         greet(System.out::println);
+
+        /**
+         * Build-in Functional Interfaces to perform common tasks
+         * Consumer Interface -  void consume(obj) -> Takes Single Arguments and returns nothing.
+         * Supplier Interface - Opposite of Consumer Interface -> obj supply() -> Takes No Arguments and returns value.
+         * Function - Obj map(obj) -> Function that map a value on differnt value.
+         * Predicate - bool test(condition) -> opration that takes a object - check and see if object satisify cretira // used for filter data
+         *
+         *
+         */
     }
 
     // Programing to printer interface
