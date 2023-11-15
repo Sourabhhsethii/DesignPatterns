3f4e1324e7b40afcfc19c3ecdc5b76dd556a2128
Sourabh Sethi <sourabhhsethii@linux.com>
2023-11-15 09:20:53 -0500

    Solve State Pattern!

---
+++ src/com/dxsys/patterns/behavioural/nov2023/state/Canvas.java
@@ -0,0 +1,48 @@
+package com.dxsys.patterns.behavioural.nov2023.state;
+
+/**
+ * How you will solve the below example!!!
+ * Think Think Think???
+ * It is Voilation of SRP & OCP!!
+ */
+public class Canvas {
+    public ToolType getToolType() {
+        return toolType;
+    }
+
+    public void setToolType(ToolType toolType) {
+        this.toolType = toolType;
+    }
+
+    private ToolType toolType;
+
+    /**
+     * We need to cpy paste & Its not extensible
+     * All the decisions  making has to be copy, lets say in next keyboard icons.
+     */
+    public void mouseDown(){
+
+        if(toolType == ToolType.SELECTION){
+            System.out.println("Selection Icon");
+        } else if(toolType == ToolType.BRUSH) {
+            System.out.println("Selection Icon");
+        }else if(toolType == ToolType.BRUSH) {
+            System.out.println("Brush Icon");
+        }
+
+    }
+
+    /**
+     * We need to cpy paste & Its not extensible
+     */
+    public void mouseUp(){
+        if(toolType == ToolType.SELECTION){
+            System.out.println("Draw dashed rectangle");
+        } else if(toolType == ToolType.BRUSH) {
+            System.out.println("Brush Icon");
+        }else if(toolType == ToolType.BRUSH) {
+            System.out.println("Erasers Icon");
+        }
+
+    }
+}
---
+++ src/com/dxsys/patterns/behavioural/nov2023/state/ToolType.java
@@ -0,0 +1,7 @@
+package com.dxsys.patterns.behavioural.nov2023.state;
+
+public enum ToolType {
+    SELECTION,
+    BRUSH,
+    ERASER
+}
