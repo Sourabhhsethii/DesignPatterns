39c7a9d304c0f86d1022a136aaed1dcb0edb1135
Sourabh Sethi <sourabhhsethii@linux.com>
2024-01-21 23:54:08 -0500

    Bird, OCP -> Practical Factory Creation Of Type of Bird

--- src/assignment/design/bird/Main.java
+++ src/assignment/design/bird/Main.java
@@ -4,6 +4,8 @@ import assignment.design.bird.entities.Bird;
 import assignment.design.bird.entities.Crow;
 import assignment.design.bird.entities.Parrot;
 import assignment.design.bird.entities.Penguin;
+import assignment.design.bird.factory.BirdCreationFatory;
+import assignment.design.bird.fly.BirdType;
 import assignment.design.bird.fly.CrowOwlyFlyingStyle;
 import assignment.design.bird.fly.ParrotSparrowFlyingStyle;
 
@@ -20,5 +22,15 @@ public class Main {
         var bird2 = new Parrot("Green", 1,2, new ParrotSparrowFlyingStyle());
         System.out.println(bird2.getColour());
         bird2.letsFly();
+
+        /**
+         * Object Creation of bird with factory Method.
+         */
+
+        var bird3 = BirdCreationFatory.getFlyingBird(BirdType.Parrot,"Green", 1,2, new ParrotSparrowFlyingStyle());
+        System.out.println(bird3.getColour());
+
+        var bird4 = BirdCreationFatory.getNonFlyingBird(BirdType.Penguin,"White",1,2);
+        System.out.println(bird4.getColour());
     }
 }
---
+++ src/assignment/design/bird/factory/BirdCreationFatory.java
@@ -0,0 +1,33 @@
+package assignment.design.bird.factory;
+
+import assignment.design.bird.entities.*;
+import assignment.design.bird.fly.BirdType;
+import assignment.design.bird.fly.Flying;
+
+public class BirdCreationFatory {
+
+    private static Bird bird;
+
+    public static Bird getFlyingBird(BirdType birdType, String colour, Integer height, Integer width, Flying flying) {
+
+        if(birdType == BirdType.Parrot){
+            bird = new Parrot(colour,height,width,flying);
+        } else if(birdType == BirdType.Crow) {
+            bird = new Crow(colour,height,width,flying);
+        } else if(birdType == BirdType.Owly){
+            bird = new Owl(colour,height,width,flying);
+        } else if(birdType == BirdType.Sparrow){
+            bird = new Sparrow(colour,height,width,flying);
+        }
+        return bird;
+    }
+
+
+    public static Bird getNonFlyingBird(BirdType birdType, String colour, Integer height, Integer width) {
+
+        if(birdType == BirdType.Penguin){
+            bird = new Penguin(colour,height,width);
+        }
+        return bird;
+    }
+}
